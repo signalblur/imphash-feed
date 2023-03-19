222e85770e8b6d6df5b7adf0c917de4b1e7a55c1
signalblurio <45216760+signalblurio@users.noreply.github.com>
2023-03-19 17:19:10 -0400

    final

--- src/main.py
+++ src/main.py
@@ -5,13 +5,11 @@ ABUSE_CH = 'https://mb-api.abuse.ch/api/v1/'
 
 def ioc_update(mal_name, imphash):
     try:
-        if os.path.isfile(mal_name + '.txt'):
-            os.system(f'rm {mal_name}.txt')
-        with open(mal_name + '.txt', 'a') as f:
-            f.write(f'{imphash}')
+        with open(mal_name + '_imphash.txt', 'a') as f:
+            f.write(f'{imphash}\n')
     except:
         pass
-    
+
 def main():
     """
     Tool to download and manage imphashes for various forms of malware.
@@ -31,7 +29,11 @@ def main():
     for tag in tags:
         data = {'query':'get_taginfo', 'tag':tag, 'limit':50}
         res = requests.post(ABUSE_CH, data=data)
-        ioc_update(res.json().get('data').get('signature'), res.json().get('data').get('imphash'))
+        #print(res.json().get('data'))
+        for x in res.json().get('data'):
+            if x.get('imphash') != None:
+                if x.get('imphash') != '':
+                    ioc_update(tag, x.get('imphash'))
 
 if __name__ == '__main__':
     main()
